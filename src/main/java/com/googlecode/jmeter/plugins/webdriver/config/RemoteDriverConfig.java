1773c3303e53b1e02520299395e6c93d02095c6a
CaptainB <bin@fit2cloud.com>
2023-03-02 12:09:13 +0800

    refactor: 减少gitdiff

--- src/main/java/com/googlecode/jmeter/plugins/webdriver/config/RemoteDriverConfig.java
+++ src/main/java/com/googlecode/jmeter/plugins/webdriver/config/RemoteDriverConfig.java
@@ -18,75 +18,71 @@ import java.util.Map;
 
 public class RemoteDriverConfig extends WebDriverConfig<RemoteWebDriver> {
 
-    private static final long serialVersionUID = 100L;
-    private static final Logger LOGGER = LoggerFactory.getLogger(RemoteDriverConfig.class);
+	private static final long serialVersionUID = 100L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(RemoteDriverConfig.class);
 
-    private static final String LOCAL_FILE_DETECTOR = "RemoteDriverConfig.general.selenium.file.detector";
-    private static final String REMOTE_CAPABILITY = "RemoteDriverConfig.general.selenium.capability";
-    private static final String REMOTE_SELENIUM_GRID_URL = "RemoteDriverConfig.general.selenium.grid.url";
+	private static final String LOCAL_FILE_DETECTOR = "RemoteDriverConfig.general.selenium.file.detector";
+	private static final String REMOTE_CAPABILITY = "RemoteDriverConfig.general.selenium.capability";
+	private static final String REMOTE_SELENIUM_GRID_URL = "RemoteDriverConfig.general.selenium.grid.url";
     private static final String BROWSER_LANGUAGE = "RemoteDriverConfig.browser.language";
 
-    @Override
-    protected RemoteWebDriver createBrowser() {
-        try {
-            RemoteWebDriver driver = new RemoteWebDriver(new URL(getSeleniumGridUrl()), createCapabilities());
-            if (isLocalFileDectedor()) {
-                driver.setFileDetector(new LocalFileDetector());
-            }
-            LOGGER.debug("Created remote web driver.");
-            return driver;
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Override
+	protected RemoteWebDriver createBrowser() {
+		try {
+			RemoteWebDriver driver = new RemoteWebDriver(new URL(getSeleniumGridUrl()), createCapabilities());
+			if (isLocalFileDectedor()) {
+				driver.setFileDetector(new LocalFileDetector());
+			}
+			LOGGER.debug("Created remote web driver.");
+			return driver;
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    Capabilities createCapabilities() {
-        // We pass the browser option instance to the remote so it knows which browser to use
-        AbstractDriverOptions<?> caps = null;
-
+	Capabilities createCapabilities() {
         setAcceptInsecureCerts(true);
-        switch (getCapability()) {
-            case CHROME:
-                caps = createChromeOptions();
-                break;
-            case EDGE:
-                caps = createEdgeOptions();
-                break;
-            case FIREFOX:
-                caps = createFirefoxOptions();
-                break;
-            case INTERNET_EXPLORER:
-                caps = createIEOptions();
-                break;
-            default:
-                throw new IllegalArgumentException("No such capability");
-        }
-        return caps;
-    }
+        // We pass the browser option instance to the remote so it knows which browser to use
+		AbstractDriverOptions<?> caps = null;
+		switch (getCapability()) {
+		case CHROME:
+			caps = createChromeOptions();
+			break;
+		case EDGE:
+			caps = createEdgeOptions();
+			break;
+		case FIREFOX:
+			caps = createFirefoxOptions();
+			break;
+		case INTERNET_EXPLORER:
+			caps = createIEOptions();
+			break;
+		default:
+			throw new IllegalArgumentException("No such capability");
+		}
+		return caps;
+	}
 
-    public RemoteCapability getCapability() {
-        return RemoteCapability.valueOf(getPropertyAsString(REMOTE_CAPABILITY));
-    }
+	public RemoteCapability getCapability() {
+		return RemoteCapability.valueOf(getPropertyAsString(REMOTE_CAPABILITY));
+	}
+	public void setCapability(RemoteCapability selectedCapability) {
+		setProperty(REMOTE_CAPABILITY, selectedCapability.name());
+	}
 
-    public void setCapability(RemoteCapability selectedCapability) {
-        setProperty(REMOTE_CAPABILITY, selectedCapability.name());
-    }
+	public String getSeleniumGridUrl() {
+		return getPropertyAsString(REMOTE_SELENIUM_GRID_URL);
+	}
+	public void setSeleniumGridUrl(String seleniumUrl) {
+		setProperty(REMOTE_SELENIUM_GRID_URL, seleniumUrl);
+	}
 
-    public String getSeleniumGridUrl() {
-        return getPropertyAsString(REMOTE_SELENIUM_GRID_URL);
-    }
-
-    public void setSeleniumGridUrl(String seleniumUrl) {
-        setProperty(REMOTE_SELENIUM_GRID_URL, seleniumUrl);
-    }
-
-    public boolean isLocalFileDectedor() {
-        return getPropertyAsBoolean(LOCAL_FILE_DETECTOR);
-    }
-
-    public void setLocalFileDetector(boolean enabled) {
-        setProperty(LOCAL_FILE_DETECTOR, enabled);
-    }
+	public boolean isLocalFileDectedor() {
+		return getPropertyAsBoolean(LOCAL_FILE_DETECTOR);
+	}
+	public void setLocalFileDetector(boolean enabled) {
+		setProperty(LOCAL_FILE_DETECTOR, enabled);
+	}
 
     public void setBrowserLanguage(String browserLanguage) {
         setProperty(BROWSER_LANGUAGE, browserLanguage);
